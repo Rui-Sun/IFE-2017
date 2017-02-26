66040b675e2ee5ad1602917e7fa3f709f0bb93f1
Rui-Sun <sunrui1992@foxmail.com>
2017-02-26 15:44:46 +0800

    use prototype function

--- Nuomi-school/task9/vue4.js
+++ Nuomi-school/task9/vue4.js
@@ -1,19 +1,25 @@
 function Vue (obj) {
 	this.el = obj.el;
 	this.data = obj.data;
-	this.set = {};
-	var container = document.getElementById(this.el.slice(1));
+	this.set();
+};
+
+Vue.prototype.set = function(){
+	var bind = {};
+	var container = document.querySelector(this.el);
 	for(let key in this.data){
 		for(let prop in this.data[key]){
-			this.set["{{"+key+"."+prop+"}}"] = this.data[key][prop];
+			bind["{{"+key+"."+prop+"}}"] = this.data[key][prop];
 		}
 	}
 	for(let i=0 ; i<container.children.length ; i++){
-		for(let prop in this.set){
-			container.children[i].innerHTML = container.children[i].innerHTML.replace(prop,this.set[prop]);
+		for(let prop in bind){
+			container.children[i].innerHTML = container.children[i].innerHTML.replace(prop,bind[prop]);
 		}
 	}
-}
+};
+
+
 
 let app = new Vue({
   el: '#app',
