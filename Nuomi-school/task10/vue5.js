2389c27d50cbf28a9561e5530c24086868e28fe0
Rui-Sun <sunrui1992@foxmail.com>
2017-02-26 19:22:57 +0800

    accomplish Vue5 task

--- Nuomi-school/task10/vue5.js
+++ Nuomi-school/task10/vue5.js
@@ -6,8 +6,8 @@ function Observer(vue){
 	var self = this;
 	for(let prop in this.data){
 		this.$watch(prop,function(){
-			console.log(arguments);
-			Vue.prototype.set.call(app);
+			// console.log(arguments);
+			Vue.prototype.set.call(app);    //订阅Vue对象实例中的所有属性，处理方法为调用Vue对象的set方法
 		});
 	}
 }
@@ -19,12 +19,6 @@ p.$watch = function(eventType,handler){
 	if(typeof this.data[eventType] === "object"){
 		search(this.queue[eventType],handler);
 	}
-	// else{
-	// 	if(!(eventType in this.handlers)) {
-	// 		this.handlers[eventType] = [];
-	// 	}
-	// 	this.handlers[eventType].push(handler);
-	// }
 	function search(childObj,handler){
 		for(let prop in childObj.data){
 			if(typeof prop === "object"){
@@ -45,7 +39,7 @@ p.emit = function(eventType){
 	var handlerArgs = Array.prototype.slice.call(arguments,0);
 	for(let i=0 ; i< this.handlers[eventType].length ; i++) {
 		// this.handlers[eventType][i].apply(this,handlerArgs);
-		this.handlers[eventType][i].apply(window,handlerArgs);
+		this.handlers[eventType][i].apply(window,handlerArgs);     //在window中调用Vue的set方法
 	}
 };
 
@@ -65,7 +59,6 @@ p.walk = function(obj){
 
 p.convert = function(key,val){
 	var self = this;
-
 	Object.defineProperty(self.data,key,{
 		enumerable: true,
 		configurable: true,
@@ -79,7 +72,7 @@ p.convert = function(key,val){
 			val = newVal;
 			for(let prop in self.handlers){
 				if(prop === key){
-					self.emit(key,val);                //发布事件
+					self.emit(key,val);                //发布事件，将属性名和最新值传回
 				}
 			}
 			if(typeof newVal === "object"){                 //考虑属性值设置为对象
@@ -91,29 +84,44 @@ p.convert = function(key,val){
 	})
 };
 
+function Vue (obj) {
+	this.el = obj.el;
+	this.data = obj.data;
+	const _init = document.querySelector(this.el).innerHTML;     //创建_init私有变量保存最初的DOM
+	this.getInit = function(){
+		return _init;
+	}
+	this.set();
+};
 
-// let app1 = new Observer({
-//   name: "sunrui",
-//   age: 25
-// });
+Vue.prototype.set = function(){
+	var container = document.querySelector(this.el);
+	container.innerHTML = this.getInit();                          //每次调用DOM渲染前将DOM恢复到最初状态
+	var bind = {};
+	for(let key in this.data){
+		for(let prop in this.data[key]){
+			bind["{{"+key+"."+prop+"}}"] = this.data[key][prop];
+		}
+	}
+	for(let i=0 ; i<container.children.length ; i++){
+		for(let prop in bind){
+			container.children[i].innerHTML = container.children[i].innerHTML.replace(prop,bind[prop]);
+		}
+	}
+};
 
-// let app2 = new Observer({
-//   university: "cugb",
-//   major: "hydrogeology"
-// });
 
-// app1.$watch('age', function(age) {
-//          console.log("我的年纪变了，现在已经是：" + age +"岁了");
-//  });
 
-// let app3 = new Observer({
-//     name: {
-//         firstName: 'rui',
-//         lastName: 'sun'
-//     },
-//     age: 25
-// });
+let app = new Vue({
+  el: '#app',
+  data: {
+    user: {
+      name: 'sunrui',
+      age: 25
+    },
+    school: 'cugb',
+    major: 'hydrogeologe'
+  }
+});
 
-// app3.$watch('name', function (newName) {
-//     console.log('我的姓名发生了变化，可能是姓氏变了，也可能是名字变了。')
-// });
+let appWatch = new Observer(app);
