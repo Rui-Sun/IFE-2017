d5e407cf365ba3250a0534b9c972d46f068512d9
Rui-Sun <sunrui1992@foxmail.com>
2017-02-28 10:33:22 +0800

    accomplish markdown img and link

--- Nuomi-school/task11/markdown.js
+++ Nuomi-school/task11/markdown.js
@@ -19,6 +19,7 @@ function show(){
 	});
 
 	//解析无序列表
+	//列表中每行和其下一行比较，若下一行比本行缩进大，则创建子列表
 	v = v.replace(/^(\s{0,3})[*+-]\s(.*)$(?=\n^(\s{0,3})[*+-]\s.*$)/gm,function(match,group1,group2,group3){
 		let levelNow = group1.length;
 		let levelNext = group3.length;
@@ -47,9 +48,10 @@ function show(){
 			}
 		}
 	});
-	v = v.replace(/^\s{0,3}[*+-]\s(.*)$/gm,"<ul><li>$1</li></ul>");
+	v = v.replace(/^\s{0,3}[*+-]\s(.*)$/gm,"<ul><li>$1</li></ul></ul></ul></ul></ul>");
 
 	//解析有序列表
+	//原理同无序列表
 	v = v.replace(/^(\s{0,3})(\d)+[\.\)]\s(.*)$(?=\n^(\s{0,3})\d+[\.\)]\s.*$)/gm,function(match,group1,group2,group3,group4){
 		let levelNow = group1.length;
 		let levelNext = group4.length;
@@ -78,9 +80,10 @@ function show(){
 			}
 		}
 	});
-	v = v.replace(/^\s{0,3}(\d)+[\.\)]\s(.*)$/gm,"<ol><li value=\"$1\">$2</li></ol>");
+	v = v.replace(/^\s{0,3}(\d)+[\.\)]\s(.*)$/gm,"<ol><li value=\"$1\">$2</li></ol></ol></ol></ol></ol>");
 
 	//解析引用
+	//先找到一个引用块（以空行结束或文本结束）创建应用div,再判断引用块内是否有子引用块
 	v = v.replace(/((^>(.|\n)+?(?=^$))|(^>(.|\n)+))/gm,function(match,group1){
 		let origin1 = match;			
 		let maxLength = 0;                   
@@ -109,7 +112,6 @@ function show(){
 
 	//解析行内代码
 	v = v.replace(/`([^`]+)(?!^.*)`/gm,"<code>$1</code>");
-
 	//解析块级代码
 		//`...`情况
 	v = v.replace(/^```$([^(```)]+)^```$/gm,"<pre><code>$1</code></pre>");
@@ -120,6 +122,34 @@ function show(){
 		origin = "<pre><code>"+origin+"</code></pre>";
 		return origin;
 	})
+
+	//解析图片
+		//无标识符
+	v = v.replace(/\!\[(.*)\]\((.+)\)/gm,"<img src=\"$2\" alt=\"$1\" /><br />");   //"<img src=\"$3\" alt=\"$1\" title=\"$4\"/>"
+		//有标识符
+	let identifiersI = []; //储存标识符内容
+	v = v.replace(/\!\[(.*)\]\[(.+)\](?=(?:.|\n)+(\[\2\]\:\s(.+)(?:\s"(.*)")?))/gm,function(match,group1,group2,group3,group4,group5){
+		identifiersI.push(group3);
+		return "<img src=\""+group4+"\" alt=\""+group1+"\" title=\""+group5+"\"/><br />";
+	});
+	for(let i=0 ; i<identifiersI.length ; i++){
+		v = v.replace(identifiersI[i],"");  //删除标识符
+	}
+	
+	//解析链接
+		//无标识符
+	v = v.replace(/[^\!]\[(.*)\]\((.+)\)/gm,"<a href=\"$2\">$1</a>");
+		//有标识符
+	let identifiersL = []; //储存标识符内容
+	v = v.replace(/[^\!]\[(.*)\]\[(.+)\](?=(?:.|\n)+(\[\2\]\:\s(.+)(?:\s"(.*)")?))/gm,function(match,group1,group2,group3,group4,group5){
+		identifiersL.push(group3);
+		return "<a href=\""+group4+"\" title=\""+group5+"\"/>"+group1+"</a>";
+	});
+	for(let i=0 ; i<identifiersL.length ; i++){
+		v = v.replace(identifiersL[i],"");  //删除标识符
+	}
+
+	v = v.replace(/^$/gm,"<br /><br />");
 	// console.log(v);
 
 	disp.innerHTML = v;
